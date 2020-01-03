be66fa47acdcefc3a1b953a84e3e52ab4538ebfe
Marion BELORGEY <mbelorge@e2r12p17.42.fr>
2020-01-03 15:53:57 +0100

    norminette ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mbelorge <mbelorge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 13:21:00 by mbelorge          #+#    #+#             */
-/*   Updated: 2019/12/04 14:23:56 by mbelorge         ###   ########.fr       */
+/*   Updated: 2020/01/03 15:46:07 by mbelorge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-size_t	ft_strlen(const char *str); 
-int     get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+int		get_next_line(int fd, char **line);
 char	*ft_strjoinfree(char const *s1, char const *s2);
 char	*ft_strchr(char *s, int c);
 char	*ft_strdup(const char *s1);
-char    *ft_substr(const char *s, unsigned int star, size_t len); 
-void	*ft_calloc(size_t count, size_t size); 
- 
+char	*ft_substr(const char *s, unsigned int star, size_t len);
+void	*ft_calloc(size_t count, size_t size);
+
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,36 +6,34 @@
 /*   By: mbelorge <mbelorge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 13:19:22 by mbelorge          #+#    #+#             */
-/*   Updated: 2019/12/04 15:07:12 by mbelorge         ###   ########.fr       */
+/*   Updated: 2020/01/03 15:46:20 by mbelorge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
 void		*ft_calloc(size_t count, size_t size)
 {
 	int		*pointeur;
 	int		s;
-	size_t i; 
-	
+	size_t	i;
+
 	s = count * size;
 	if (s == 0)
 		s = 1;
 	pointeur = malloc(s);
 	if (pointeur == NULL)
-		return (0); 
-
+		return (0);
 	i = 0;
 	while (i < size)
 	{
-		((unsigned char *)pointeur)[i] =  '\0' ;
+		((unsigned char *)pointeur)[i] = '\0';
 		i++;
 	}
 	return (pointeur);
 }
 
-size_t	ft_strlen(const char *str)
+size_t		ft_strlen(const char *str)
 {
 	int i;
 
@@ -45,11 +43,11 @@ size_t	ft_strlen(const char *str)
 	return (i);
 }
 
-char			*ft_substr(char const *s, unsigned int star, size_t len)
+char		*ft_substr(char const *s, unsigned int star, size_t len)
 {
-	char		*resultat;
-	size_t		i;
-	size_t		j;
+	char	*resultat;
+	size_t	i;
+	size_t	j;
 
 	resultat = NULL;
 	i = 0;
@@ -68,11 +66,10 @@ char			*ft_substr(char const *s, unsigned int star, size_t len)
 		i++;
 	}
 	resultat[j] = '\0';
-	
 	return (resultat);
 }
 
-char			*ft_strchr(char *s, int c)
+char		*ft_strchr(char *s, int c)
 {
 	if (!s || !c)
 		return (NULL);
@@ -83,4 +80,22 @@ char			*ft_strchr(char *s, int c)
 		s++;
 	}
 	return (NULL);
+}
+
+char		*ft_strdup(const char *s1)
+{
+	char	*resultat;
+	int		i;
+
+	resultat = (char*)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!resultat)
+		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		resultat[i] = s1[i];
+		i++;
+	}
+	resultat[i] = '\0';
+	return (resultat);
 }
